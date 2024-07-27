f0fc062205fe5d94285de0454243c6922736fc56
Esumei Moses <archer.me24@gmail.com>
2024-07-27 04:10:20 +0000

    class in JS

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,10 @@
+/**
+ * blueprint of classRoom.
+ */
+export default class ClassRoom {
+  /*maxStudentsSize - No. of students in class*/
+
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
